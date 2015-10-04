38aa51f0aa8a6773827e43f479b2bbac56a24df8
Gabriel T. Sharp <osirisgothra@hotmail.com>
2015-10-04 00:09:55 -0400

    added compositor, reformated compbind preparing for rewrite of it, touched axtool

--- lib/axtool.py
+++ lib/axtool.py
@@ -2,6 +2,7 @@
 
 # please use python3 or pypy3
 
+import axlib.libaxtool
 
 """
 axtool.py
@@ -12,6 +13,3 @@ if (__name__ == "__main__"):
 	pass
 else:
 	print("You must run this file as a program. It is not intended to be loaded as a module!")
-
-
-
